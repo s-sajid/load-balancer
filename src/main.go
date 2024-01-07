ff2288233007637bd192284cebcdb7e5d820dbaa
s-sajid <88870507+s-sajid@users.noreply.github.com>
2024-01-06 19:33:18 -0700

    Add newServer

--- src/main.go
+++ src/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "net/http/httputil"
+import (
+	"net/http/httputil"
+	"net/url"
+)
 
 type server struct {
 	addr  string
 	proxy *httputil.ReverseProxy
+}
+
+func newServer(addr string) *server {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+
+	return &server{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
 }
